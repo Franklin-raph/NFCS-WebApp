cf00c73a28b850e8bb58f6d896ffc263776f729e
Franklin-raph <igboekwulusifranklin@gmail.com>
2021-11-02 21:33:02 +0100

    worked on the contact

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ $('.counter').counterUp({
   $(window).on('load', function(){
     new WOW().init(); 
 });
+
+// const nameField = document.querySelector("#name")
+// const emailField = document.querySelector("#email")
+// const messageField = document.querySelector("#message")
+// const submitBtn = document.querySelector("#sendMailBtn")
+
+
+
+// submitBtn.addEventListener("click", (e) => {
+//   e.preventDefault()
+//   console.log(nameField.value)
+//   console.log(emailField)
+//   console.log(messageField)
+// })
