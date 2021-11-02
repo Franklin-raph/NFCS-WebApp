410f78f8a0045632c2871a4311dc17c41443e8fe
Franklin-raph <igboekwulusifranklin@gmail.com>
2021-11-02 14:00:55 +0100

    resolved a merge conflict

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 $('.counter').counterUp({
     delay:20,
     time: 9000
-<<<<<<< HEAD
   });
   $(window).on('load', function(){
     new WOW().init(); 
 });
-=======
-  })
- 
->>>>>>> f79351d6e108138c9c07f858eb57545e44e79306
