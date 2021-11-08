dd5d624e214b11612394ce3ad65663015bfb99cf
Franklin-raph <igboekwulusifranklin@gmail.com>
2021-11-08 19:14:33 +0100

    fixed a bug

--- js/main.js
+++ js/main.js
@@ -27,12 +27,12 @@ $('.owl-carousel').owlCarousel({
   }
 })
 
-Email.send({
-    SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
-    To : 'them@website.com',
-    From : "you@isp.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-}).then(
-  message => alert(message)
-);
+// Email.send({
+//     SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+//     To : 'them@website.com',
+//     From : "you@isp.com",
+//     Subject : "This is the subject",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );
